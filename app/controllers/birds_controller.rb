a906789900f722d5edb6f925496302750e4b35b4
Paige Miles <paige@lucas-miles.com>
2020-10-13 13:59:46 +0000

    Automatically backed up by Learn

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,6 @@
 class BirdsController < ApplicationController
   def index
     @birds = Bird.all
-    render json: { birds: @birds, messages: ['Hello Birds', 'Goodbye Birds'] }
+    render json: 'Remember that JSON is just object notation converted to string data, so strings also work here'
   end
 end
